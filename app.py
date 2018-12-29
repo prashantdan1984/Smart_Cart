10f056bc9944b05d9792dde4d726d8ff68371782
mr-robot0 <vptl185@gmail.com>
2018-12-29 12:37:15 +0530

    0.0.1

---
+++ app.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+from os import popen as p
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+     ret, frame = cap.read()
+     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+  
+     cv2.imshow('frame',gray)
+     cv2.imwrite("bar.png",frame)
+
+     print (p("zbarimg bar.png").readlines())
+     
+     if cv2.waitKey(1) & 0xFF == ord('q'):
+         break
+
+cap.release()
+cv2.destroyAllWindows()
